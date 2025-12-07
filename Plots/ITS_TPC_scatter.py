3caa9d94be6728f58c9677c1dadb4887be713fff
kirafo <oberlekira@gmail.com>
2025-12-07 10:51:42 +0100

    code for initial plots

---
+++ Plots/ITS_TPC_scatter.py
@@ -0,0 +1,104 @@
+#classnames in kaons {'data;1': 'TTree'}
+#TTrees in directory: ['index', 'pT', 'eta', 'phi', 'p', 'dEdxITS', 'NclusterPIDTPC', 'dEdxTPC', 'ITSclsMap', 'NclusterTPC', 'NcrossedRowsTPC', 'NFindableTPC']
+
+#Explaination of the branches in the Data:
+#pT - transverse momenta of the particles
+#eta - pseudorapidity
+#phi - phi of the track, in radians withing [0, 2pi)
+#dEdxITS - the energy loss in the ITS
+#ITSclsMap - 
+#NclusterPIDTPC - 
+#NclusterTPC - number of TPC clusters
+#NcrossedRowsTPC - number of crossed TPC rows
+#NFindableTPC - findable TPC clusters for this track geometry
+#dEdxTPC - energy loss in the TPC
+
+
+
+import uproot
+import matplotlib.pyplot as plt
+
+
+kaons = uproot.open("Kaons.root")
+pions = uproot.open("Pions.root")
+protons = uproot.open("Protons.root")
+electrons = uproot.open("Electrons.root")
+deuterons = uproot.open("Deuterons.root")
+
+myKaonTree = kaons["data;1"]
+myPionTree = pions["data;1"]
+myProtonTree = protons["data;1"]
+myElectronTree = electrons["data;1"]
+myDeuteronTree = deuterons["data;1"]
+
+kaonBranches = myKaonTree.arrays()
+pionBranches = myPionTree.arrays()
+protonBranches = myProtonTree.arrays()
+electronBranches = myElectronTree.arrays()
+deuteronBranches = myDeuteronTree.arrays()
+
+
+yk_00 = kaonBranches ['dEdxITS']
+yk_11 = kaonBranches ['dEdxTPC']
+xk = kaonBranches ['pT']
+
+ypi_00 = pionBranches['dEdxITS']
+ypi_11 = pionBranches['dEdxTPC']
+xpi = pionBranches ['pT']
+
+yp_00 = protonBranches['dEdxITS']
+yp_11 = protonBranches['dEdxTPC']
+xp = protonBranches ['pT']
+
+ye_00 = electronBranches['dEdxITS']
+ye_11 = electronBranches ['dEdxTPC']
+xe = electronBranches['pT']
+
+yd_00 = deuteronBranches['dEdxITS']
+yd_11 = deuteronBranches['dEdxTPC']
+xd = deuteronBranches['pT']
+
+
+fig, ax = plt.subplots(ncols=2, nrows=1, figsize=(10, 5), layout="constrained")
+
+
+ax[0].scatter(xpi, ypi_00, 0.1, c='navy', alpha = 0.4)
+ax[0].scatter(xp, yp_00, 0.1, c='navy', alpha = 0.4)
+ax[0].scatter(xk, yk_00, 0.1, c='navy', alpha = 0.4)
+ax[0].scatter(xd, yd_00, 0.1, c='navy', alpha = 0.4, label='other')
+ax[0].scatter(xe, ye_00, 0.1, c='red', alpha = 0.4, label='electrons')
+
+ax[0].set_title('dEdxITS')
+ax[0].set_ylabel('dEdxITS')
+ax[0].set_xlabel('pT')
+
+ax[0].set_xscale('log')
+ax[0].set_yscale('log')
+ax[0].grid(alpha = 0.4)
+ax[0].grid(which = "minor", alpha = 0.4)
+ax[0].set_axisbelow(True)
+ax[0].minorticks_on()
+ax[0].legend(markerscale=10)
+
+
+
+ax[1].scatter(xpi, ypi_11, 0.1, c='navy', alpha = 0.4)
+ax[1].scatter(xp, yp_11, 0.1, c='navy', alpha = 0.4)
+ax[1].scatter(xk, yk_11, 0.1, c='navy', alpha = 0.4)
+ax[1].scatter(xd, yd_11, 0.1, c='navy', alpha = 0.4, label='other')
+ax[1].scatter(xe, ye_11, 0.1, c='red', alpha = 0.4, label='electrons')
+
+ax[1].set_title('dEdxTPC')
+ax[1].set_ylabel('dEdxTPC')
+ax[1].set_xlabel('pT')
+
+ax[1].set_xscale('log')
+ax[1].set_yscale('log')
+ax[1].grid(alpha = 0.4)
+ax[1].grid(which = "minor", alpha = 0.4)
+ax[1].set_axisbelow(True)
+ax[1].minorticks_on()
+ax[1].legend(markerscale=10)
+
+
+plt.show()
---
+++ Plots/initial_plots.py
@@ -0,0 +1,95 @@
+import uproot
+import matplotlib.pyplot as plt
+
+# --- 1. Load Data Directly ---
+# We use library='np' so we can plot immediately without extra conversion steps
+kaons     = uproot.open("Kaons.root")["data;1"].arrays(library='np')
+pions     = uproot.open("Pions.root")["data;1"].arrays(library='np')
+protons   = uproot.open("Protons.root")["data;1"].arrays(library='np')
+electrons = uproot.open("Electrons.root")["data;1"].arrays(library='np')
+deuterons = uproot.open("Deuterons.root")["data;1"].arrays(library='np')
+
+# --- 2. Configuration for Histogram Plots ---
+# Defining all particles and their colors centrally for the histograms
+HIST_PARTICLES = [
+    ('Protons',   protons,   'blue'),
+    ('Pions',     pions,     'green'),
+    ('Electrons', electrons, 'red'),
+    ('Kaons',     kaons,     'orange'),  # Added
+    ('Deuterons', deuterons, 'purple'), # Added
+]
+
+# --- 3. Create Plot Layout ---
+fig, axes = plt.subplots(ncols=2, nrows=2, figsize=(10, 8), layout="constrained")
+
+# Define common look for scatters
+scatter_style = {'s': 0.1, 'alpha': 0.4} 
+
+# ---------------------------------------------------------
+# Plot [0,0]: pT vs dEdxITS (Scatter)
+# ---------------------------------------------------------
+ax00 = axes[0,0]
+ax00.scatter(pions['pT'],     pions['dEdxITS'],     c='navy', **scatter_style)
+ax00.scatter(protons['pT'],   protons['dEdxITS'],   c='navy', **scatter_style)
+ax00.scatter(kaons['pT'],     kaons['dEdxITS'],     c='navy', **scatter_style)
+ax00.scatter(deuterons['pT'], deuterons['dEdxITS'], c='navy', label='other', **scatter_style)
+ax00.scatter(electrons['pT'], electrons['dEdxITS'], c='red',  label='electrons', **scatter_style)
+
+ax00.set_title('dEdxITS')
+ax00.set_ylabel('dEdxITS')
+ax00.set_xlabel('pT')
+ax00.set_xscale('log')
+ax00.set_yscale('log')
+ax00.legend(markerscale=10)
+
+# ---------------------------------------------------------
+# Plot [0,1]: NcrossedRowsTPC (Histogram - All Particles)
+# ---------------------------------------------------------
+ax01 = axes[0,1]
+for name, data_obj, color in HIST_PARTICLES:
+    ax01.hist(data_obj['NcrossedRowsTPC'], bins=150, color=color, label=name, alpha=0.7)
+
+ax01.set_title('NcrossedRowsTPC')
+ax01.set_ylabel('NcrossedRowsTPC Counts')
+ax01.set_yscale('log')
+ax01.legend()
+
+# ---------------------------------------------------------
+# Plot [1,0]: NclusterPIDTPC (Histogram - All Particles)
+# ---------------------------------------------------------
+ax10 = axes[1,0]
+for name, data_obj, color in HIST_PARTICLES:
+    ax10.hist(data_obj['NclusterPIDTPC'], bins=150, color=color, label=name, alpha=0.7)
+
+ax10.set_title('NclusterPIDTPC')
+ax10.set_ylabel('NclusterPIDTPC Counts')
+ax10.set_xlabel('pT')
+ax10.legend()
+
+# ---------------------------------------------------------
+# Plot [1,1]: pT vs dEdxTPC (Scatter)
+# ---------------------------------------------------------
+ax11 = axes[1,1]
+ax11.scatter(pions['pT'],     pions['dEdxTPC'],     c='navy', **scatter_style)
+ax11.scatter(protons['pT'],   protons['dEdxTPC'],   c='navy', **scatter_style)
+ax11.scatter(kaons['pT'],     kaons['dEdxTPC'],     c='navy', **scatter_style)
+ax11.scatter(deuterons['pT'], deuterons['dEdxTPC'], c='navy', label='other', **scatter_style)
+ax11.scatter(electrons['pT'], electrons['dEdxTPC'], c='red',  label='electrons', **scatter_style)
+
+ax11.set_title('dEdxTPC')
+ax11.set_ylabel('dEdxTPC')
+ax11.set_xlabel('pT')
+ax11.set_xscale('log')
+ax11.set_yscale('log')
+ax11.legend(markerscale=10)
+
+# ---------------------------------------------------------
+# Global Styling (Grid)
+# ---------------------------------------------------------
+for ax in axes.flat:
+    ax.grid(alpha=0.4)
+    ax.grid(which="minor", alpha=0.4)
+    ax.set_axisbelow(True)
+    ax.minorticks_on()
+
+plt.show()
